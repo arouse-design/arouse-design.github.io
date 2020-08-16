b2dbea78f4661c384a3d8a22d6199c12578bd99d
arouse-design <ar.designs1005@gmail.com>
2020-08-16 20:46:09 +0100

    change text colour and background colour

--- polyfills-es2015.js
+++ polyfills-es2015.js
@@ -6485,8 +6485,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! F:\AmyWeb\amy-app\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! F:\AmyWeb\amy-app\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\Nogdo\Desktop\amy-app (1)\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\Nogdo\Desktop\amy-app (1)\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
--- polyfills-es5.js
+++ polyfills-es5.js
@@ -20734,7 +20734,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
   /***/
   function _(module, exports, __webpack_require__) {
     __webpack_require__(
-    /*! F:\AmyWeb\amy-app\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-polyfills.js */
+    /*! C:\Users\Nogdo\Desktop\amy-app (1)\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
 
     __webpack_require__(
@@ -20742,15 +20742,15 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     "./node_modules/zone.js/dist/zone-legacy.js");
 
     __webpack_require__(
-    /*! F:\AmyWeb\amy-app\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */
+    /*! C:\Users\Nogdo\Desktop\amy-app (1)\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
     __webpack_require__(
-    /*! F:\AmyWeb\amy-app\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-jit-polyfills.js */
+    /*! C:\Users\Nogdo\Desktop\amy-app (1)\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-jit-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js");
 
     module.exports = __webpack_require__(
-    /*! F:\AmyWeb\amy-app\src\polyfills.ts */
+    /*! C:\Users\Nogdo\Desktop\amy-app (1)\src\polyfills.ts */
     "./src/polyfills.ts");
     /***/
   }
